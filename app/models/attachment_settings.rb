0fb6deb3cf0fb8e29918b8db871d835f2c0da16a
Dan Schaub <dan.s@betterment.com>
2017-05-02 14:18:41 -0400

    no yaml

--- app/models/attachment_settings.rb
+++ app/models/attachment_settings.rb
@@ -2,30 +2,59 @@ class AttachmentSettings
   include Singleton
 
   class << self
-    delegate :storage_settings, :max_size, to: :instance
+    delegate :storage_settings, :max_size, :attachments_enabled?, to: :instance
+  end
+
+  def attachments_enabled?
+    s3_enabled? || local_enabled?
   end
 
   def storage_settings
-    if settings[:s3].present?
-      { storage: :s3 }.merge(settings[:s3])
-    elsif settings[:local].present?
-      settings[:local]
+    if s3_enabled?
+      s3_settings
     else
-      raise "Sorry, we can only configure paperclip with local or s3 storage right now. See attachments.yml for details."
+      local_settings
     end
   end
 
   def max_size
-    settings['max_size'].kilobytes
+    ENV['ATTACHMENT_MAX_SIZE'] || 512.kilobytes
   end
 
   private
 
-  def settings
-    @settings ||= HashWithIndifferentAccess.new(YAML.load(interpolated_settings_file)[Rails.env])
+  def s3_enabled?
+    storage_strategy == 's3' && s3_settings_valid?
   end
 
-  def interpolated_settings_file
-    ERB.new(File.read(Rails.root.join('config/attachments.yml'))).result
+  def local_enabled?
+    storage_strategy == 'local'
+  end
+
+  def local_settings
+    {
+      path: ENV['LOCAL_UPLOAD_PATH'] || ':rails_root/public/system/:class/:attachment/:id_partition/:style/:filename'
+    }
+  end
+
+  def s3_settings
+    {
+      storage: :s3,
+      s3_credentials: {
+        access_key_id: ENV['S3_ACCESS_KEY_ID'],
+        secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+      },
+      bucket: ENV['S3_ATTACHMENT_BUCKET'],
+      s3_region: ENV['S3_REGION'] || 'us-east-1',
+      path: ENV['S3_ATTACHMENT_PATH'] || ':class/:attachment/:id_partition/:style/:filename'
+    }
+  end
+
+  def s3_settings_valid?
+    ENV['S3_ACCESS_KEY_ID'].present? && ENV['S3_SECRET_ACCESS_KEY'].present? && ENV['S3_ATTACHMENT_BUCKET'].present?
+  end
+
+  def storage_strategy
+    ENV['ATTACHMENT_STORAGE']
   end
 end
